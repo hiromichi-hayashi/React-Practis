7303446df5a91b34dc17a2fa3653e199b59c81fb
hiromichi-hayashi <75676003+hiromichi-hayashi@users.noreply.github.com>
2021-06-10 00:10:00 +0900

    memo化

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useCallback ,useMemo} from "react";
 import { ChildArea } from "./ChildArea";
 
 export default function App() {
   const [text, setText] = useState("");
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const onChangeText = (e) => {
     setText(e.target.value);
@@ -14,11 +14,35 @@ export default function App() {
     setOpen(!open);
   };
 
+  //入力時の再レンダリングの抑制
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+/*
+  useMemo 変数のメモ化複雑な変数をメモ化することで再レンダリングを減らせる
+
+  const temp = useMemo(()=>{
+    1+3
+  },[]);
+
+  console.log(temp);
+*/
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
+
+/*
+レンダリングの仕様
+
+・stateが更新されたコンポーネントは再レンダリングされる
+・propsが変更せれたコンポーネントも再レンダリングされる
+・再レンダリングされたコンポーネント配下の子要素も再レンダリングされてしまう
+
+*/
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,26 @@
-export const ChildArea = ({ open }) => {
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+//memo化をすることで子要素の再レンダリングを減らす
+export const ChildArea = memo(({ open, onClickClose }) => {
+  //文字が入力された時もレンダリングされる
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (
-        <div>
+        <div style={style}>
           <p>child</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
