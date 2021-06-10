a66915631da5df3714a3d0d2c50fb83dc3003cb2
hiromichi-hayashi <75676003+hiromichi-hayashi@users.noreply.github.com>
2021-06-10 19:41:26 +0900

    react-ruter-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useCallback, useMemo } from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { ChildArea } from "./ChildArea";
 import { InlienStyle } from "./component/InlineStyle";
 import { CssModules } from "./component/CssModules";
+import { Home } from "./Router/Home";
+import { Page1 } from "./Router/Page1";
+import { Page2 } from "./Router/Page2";
+import { Page1DetailA } from "./Router/Page1DetailA";
+import { Page1DetailB } from "./Router/Page1DetailB";
 
 export default function App() {
   const [text, setText] = useState("");
@@ -31,13 +37,45 @@ export default function App() {
 */
 
   return (
-    <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <button onClick={onClickOpen}>表示</button>
-      <InlienStyle />
-      <CssModules />
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+          <input value={text} onChange={onChangeText} />
+          <button onClick={onClickOpen}>表示</button>
+          <ChildArea open={open} onClickClose={onClickClose} />
+        </Route>
+        <Route
+          path="/page1"
+          //{ match: { url } }＝/Page1 ページ遷移するときの同じURLを表記
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+                <InlienStyle />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
+        <Route path="/page2">
+          <Page2 />
+          <CssModules />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
@@ -47,5 +85,15 @@ export default function App() {
 ・stateが更新されたコンポーネントは再レンダリングされる
 ・propsが変更せれたコンポーネントも再レンダリングされる
 ・再レンダリングされたコンポーネント配下の子要素も再レンダリングされてしまう
+
+
+Ruteing
+
+・react-ruter-domを使用
+
+・Link aタグと同じ意味
+・Switch　ページ遷移するときのurlを変更できる
+・Route　どのページに飛ぶかを指定
+
 
 */
